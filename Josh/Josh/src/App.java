50c3db39d2dc349bdf67225faaebd8b4fd139ac5
Josh <144963859+JoshuaBarrantesMena@users.noreply.github.com>
2024-02-28 16:23:53 -0600

    new class
    creacion de una nueva clase Game

--- Josh/Josh/src/App.java
+++ Josh/Josh/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Game game1 = new Game("Minecraft", 0.24f, "Sandbox", true);
+
+        game1.printGame();
     }
 }
---
+++ Josh/Josh/src/Game.java
@@ -0,0 +1,33 @@
+public class Game {
+    private String name;
+    private float gSize;
+    private String gender;
+    private Boolean isOnline;
+
+    public Game(String pName, float pGSize, String pGender, Boolean pIsOnline){
+        this.name = pName; 
+        this.gSize = pGSize;
+        this.gender = pGender;
+        this.isOnline = pIsOnline;
+    }
+
+    public void setName(String pName){
+        this.name = pName; 
+    }
+
+    public void setGSize(float pGSize){
+        this.gSize = pGSize;
+    }
+
+    public void setGender(String pGender){
+        this.gender = pGender;
+    }
+
+    public void setisOnline(Boolean pIsOnline){
+        this.isOnline = pIsOnline;
+    }
+
+    public void printGame(){
+        System.out.println("Nombre: " + name + "\nEspacio en GB: " + gSize + "\nGenero: " + gender + "Es online: " + isOnline);
+    }
+}
